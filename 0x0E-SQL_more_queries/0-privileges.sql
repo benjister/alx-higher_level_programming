30e2122e634d819d9a5e836b3b414fc855489979
root <root@kali.kali>
2023-08-16 18:35:43 -0400

    commited changes

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
-CREATE USER 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_2'@'localhost';
-cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
